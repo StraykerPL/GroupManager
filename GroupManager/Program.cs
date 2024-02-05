a9d25473f1f7f5fe6f89e7e404ea363279015dc7
Strayker <45916090+StraykerPL@users.noreply.github.com>
2024-02-05 13:16:13 +0100

    Add Blazor project files with MudBlazor library

---
+++ GroupManager/Program.cs
@@ -0,0 +1,17 @@
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
