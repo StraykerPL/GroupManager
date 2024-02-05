e66de2b4d5ec9e6432cdd8e084d8923b6393bcce
Strayker <45916090+StraykerPL@users.noreply.github.com>
2024-02-05 23:41:47 +0100

    Add buttons header for Groups Display component

---
+++ GroupManager.Data/Constants/GroupIdentifiersValues.cs
@@ -0,0 +1,8 @@
+﻿namespace GroupManager.Data.Constants
+{
+    public static class GroupIdentifiersValues
+    {
+        public const string UsersDefaultGroup = "UsersList";
+        public const string DefaultSelectedGroup = "Grupa 1";
+    }
+}
