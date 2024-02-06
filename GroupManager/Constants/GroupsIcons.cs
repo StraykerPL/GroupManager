8a07391422da67fbc44601d1ed50eefb8c48c928
Strayker <45916090+StraykerPL@users.noreply.github.com>
2024-02-06 08:47:55 +0100

    Add icons display to header buttons in Groups Display component

---
+++ GroupManager/Constants/GroupsIcons.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor;
+
+namespace GroupManager.Constants
+{
+    public sealed class GroupsIcons
+    {
+        public readonly IReadOnlyDictionary<string, string> GroupsIconsList = new Dictionary<string, string>()
+        {
+            { "Grupa 1", Icons.Material.Filled.Star },
+            { "Grupa 2", Icons.Material.Filled.Category },
+            { "Grupa 3", Icons.Material.Filled.TagFaces },
+            { "Grupa 4", Icons.Material.Filled.Cloud },
+            { "Grupa 5", Icons.Material.Filled.Rectangle },
+        };
+    }
+}
